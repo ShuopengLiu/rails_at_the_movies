ad387b1ebdf4609c8ac0c4a7f99f2c3128aec189
ShuopengLiu <shuopengliu@gmail.com>
2021-02-04 09:11:44 -0600

    Modified routes.rb to use resources and added Movies controller/view functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,12 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all
+    # @movies is passed to the view for Movies#index
   end
 
   def show
+    # params[:id] is the value after the forward slash in the url:
+    @movie = Movie.find(params[:id])
+    # @movies is passed to the view for Movies#show
   end
 end
